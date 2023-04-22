d7a40fbbce2930bcd57fb790386f3398b6a700de
Carlos flores <scarlo1@hotmail.com>
2023-04-21 19:42:31 -0500

    implement sticky header

--- components/Header.js
+++ components/Header.js
@@ -4,37 +4,39 @@ import { MagnifyingGlassIcon, PlusCircleIcon  } from '@heroicons/react/24/outlin
 import { HomeIcon } from "@heroicons/react/24/solid";
 export default function Header() {
   return (
-    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
+    <div className="shadow-sm border-b sticky top-0 bg-white z-30">
+      <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
           {/* Left */}
-            <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-                <Image
-                 src="https://1000marcas.net/wp-content/uploads/2019/11/Logo-Instagram.png"
-                 alt="Instagram"
-                 layout="fill"
-                 className="object-contain"
-                 />
-            </div>
-            <div className="cursor-pointer h-24 w-10 relative lg:hidden">
-                <Image
-                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/800px-Instagram_logo_2016.svg.png"
-                 alt="Instagram"
-                 layout="fill"
-                 className="object-contain"
-                 />
-            </div>
-            {/* Middle */}
-            <div className="relative mt-1">
+          <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+              <Image
+                src="https://1000marcas.net/wp-content/uploads/2019/11/Logo-Instagram.png"
+                alt="Instagram"
+                layout="fill"
+                className="object-contain"
+                />
+          </div>
+          <div className="cursor-pointer h-24 w-10 relative lg:hidden">
+              <Image
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/800px-Instagram_logo_2016.svg.png"
+                alt="Instagram"
+                layout="fill"
+                className="object-contain"
+                />
+          </div>
+          {/* Middle */}
+          <div className="relative mt-1">
               <div className="absolute top-2 left-2">
                 <MagnifyingGlassIcon className="h-5 text-gray-500" />
               </div>
               <input type="text" placeholder="Search" className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md" />
-            </div>
-            {/* Right */}
-            <div className="flex space-x-4 items-center">
+          </div>
+          {/* Right */}
+          <div className="flex space-x-4 items-center">
               <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
               <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
               <img src="https://yt3.googleusercontent.com/ytc/AL5GRJXhi1mm69MuHnk_n8-46VEFrMU8SelQCT3-Vv5H2A=s900-c-k-c0x00ffffff-no-rj" alt="user-image" className="h-10 rounded-full cursor-pointer" />
-            </div>
-        </div>
+          </div>
+      </div>
+    </div>
   )
 }
