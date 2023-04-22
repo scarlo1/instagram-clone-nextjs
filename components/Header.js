50dc296e571f941537933fab4d91a1f1db9c2ed5
Carlos flores <scarlo1@hotmail.com>
2023-04-21 19:25:23 -0500

    add the menu and profile image at the right side of the header

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,10 @@
 import Image from 'next/image';
 import React from 'react';
-import { MagnifyingGlassIcon  } from '@heroicons/react/24/outline';
+import { MagnifyingGlassIcon, PlusCircleIcon  } from '@heroicons/react/24/outline';
+import { HomeIcon } from "@heroicons/react/24/solid";
 export default function Header() {
   return (
-    <div className="flex items-center justify-between max-w-6xl">
+    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
           {/* Left */}
             <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
                 <Image
@@ -29,7 +30,11 @@ export default function Header() {
               <input type="text" placeholder="Search" className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md" />
             </div>
             {/* Right */}
-            <h1>Right sides</h1>
+            <div className="flex space-x-4 items-center">
+              <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+              <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+              <img src="https://yt3.googleusercontent.com/ytc/AL5GRJXhi1mm69MuHnk_n8-46VEFrMU8SelQCT3-Vv5H2A=s900-c-k-c0x00ffffff-no-rj" alt="user-image" className="h-10 rounded-full cursor-pointer" />
+            </div>
         </div>
   )
 }
