13c229b328105c103e4d294ff19354d91d63234f
Carlos flores <scarlo1@hotmail.com>
2023-04-29 11:25:21 -0500

    get the session and modify the header component with google data

--- components/Header.js
+++ components/Header.js
@@ -2,41 +2,62 @@ import Image from 'next/image';
 import React from 'react';
 import { MagnifyingGlassIcon, PlusCircleIcon  } from '@heroicons/react/24/outline';
 import { HomeIcon } from "@heroicons/react/24/solid";
+import { useSession, signIn, signOut } from 'next-auth/react';
+
 export default function Header() {
+  const {data: session} = useSession();
+  console.log(session);
   return (
     <div className="shadow-sm border-b sticky top-0 bg-white z-30">
       <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
-          {/* Left */}
-          <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-              <Image
-                src="https://1000marcas.net/wp-content/uploads/2019/11/Logo-Instagram.png"
-                alt="Instagram"
-                layout="fill"
-                className="object-contain"
-                />
+        {/* Left */}
+        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+          <Image
+            src="https://1000marcas.net/wp-content/uploads/2019/11/Logo-Instagram.png"
+            alt="Instagram"
+            layout="fill"
+            className="object-contain"
+          />
+        </div>
+        <div className="cursor-pointer h-24 w-10 relative lg:hidden">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/800px-Instagram_logo_2016.svg.png"
+            alt="Instagram"
+            layout="fill"
+            className="object-contain"
+          />
+        </div>
+        {/* Middle */}
+        <div className="relative mt-1">
+          <div className="absolute top-2 left-2">
+            <MagnifyingGlassIcon className="h-5 text-gray-500" />
           </div>
-          <div className="cursor-pointer h-24 w-10 relative lg:hidden">
-              <Image
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/800px-Instagram_logo_2016.svg.png"
-                alt="Instagram"
-                layout="fill"
-                className="object-contain"
-                />
-          </div>
-          {/* Middle */}
-          <div className="relative mt-1">
-              <div className="absolute top-2 left-2">
-                <MagnifyingGlassIcon className="h-5 text-gray-500" />
-              </div>
-              <input type="text" placeholder="Search" className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md" />
-          </div>
-          {/* Right */}
-          <div className="flex space-x-4 items-center">
-              <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+          />
+        </div>
+        {/* Right */}
+        <div className="flex space-x-4 items-center">
+          <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          {session ? (
+            <>
               <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-              <img src="https://yt3.googleusercontent.com/ytc/AL5GRJXhi1mm69MuHnk_n8-46VEFrMU8SelQCT3-Vv5H2A=s900-c-k-c0x00ffffff-no-rj" alt="user-image" className="h-10 rounded-full cursor-pointer" />
-          </div>
+              <Image
+                width="40"
+                height="6"
+                onClick={signOut}
+                src={session.user.image}
+                alt="user-image"
+                className="h-10 rounded-full cursor-pointer"
+              />
+            </>
+          ) : (
+            <button onClick={signIn}>Sign in</button>
+          )}
+        </div>
       </div>
     </div>
-  )
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ['1000marcas.net', 'upload.wikimedia.org', 'superviral.com.au', 'socodigital.com'],},
+  images: { domains: ['1000marcas.net', 'upload.wikimedia.org', 'superviral.com.au', 'socodigital.com', 'lh3.googleusercontent.com'],},
 }
 
 module.exports = nextConfig
