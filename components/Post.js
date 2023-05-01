1af7aba022ac7a323434003e20747137be3eaaa5
Carlos flores <scarlo1@hotmail.com>
2023-04-30 22:43:35 -0500

    add comment to the firebase database

--- components/Post.js
+++ components/Post.js
@@ -1,40 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { db } from '../firebase';
 import { EllipsisHorizontalIcon } from "@heroicons/react/24/solid";
 import { BookmarkIcon, ChatBubbleOvalLeftEllipsisIcon , HeartIcon, FaceSmileIcon  } from "@heroicons/react/24/outline";
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
 import { useSession } from "next-auth/react";
 
 export default function Post({img, userImg, caption, username, id}) {
   const { data: session } = useSession();
+  const [comment, setComment] = useState("");
+  async function sendComment(event){
+    event.preventDefault();
+    const commentToSend = comment;
+    setComment("")
+    await addDoc(collection(db, "posts", id, "comments"), {
+      comment: commentToSend,
+      username:session.user.username,
+      userImage: session.user.image,
+      timestamp:serverTimestamp(),
+    })
+  }
   return (
     <div className="bg-white my-7 border rounded-md">
-        {/* Post Header */}
-        <div className="flex items-center p-5">
-            <img className="h-12 rounded-full object-cover border p-1 mr-3" src={userImg} alt={username} />
-            <p className="font-bold flex-1">{username}</p>
-            <EllipsisHorizontalIcon className="h-5" />
-        </div>
-        {/* Post Image */}
-        <img className="object-cover w-full" src={img} alt="" />
-        {/* Post Buttons */}
-        {session && (
-          <div className="flex justify-between px-4 pt-4">
-            <div className="flex space-x-4">
-              <HeartIcon className="btn" />
-              <ChatBubbleOvalLeftEllipsisIcon  className="btn" />
-            </div>
-            <BookmarkIcon className="btn" />
+      {/* Post Header */}
+      <div className="flex items-center p-5">
+        <img
+          className="h-12 rounded-full object-cover border p-1 mr-3"
+          src={userImg}
+          alt={username}
+        />
+        <p className="font-bold flex-1">{username}</p>
+        <EllipsisHorizontalIcon className="h-5" />
+      </div>
+      {/* Post Image */}
+      <img className="object-cover w-full" src={img} alt="" />
+      {/* Post Buttons */}
+      {session && (
+        <div className="flex justify-between px-4 pt-4">
+          <div className="flex space-x-4">
+            <HeartIcon className="btn" />
+            <ChatBubbleOvalLeftEllipsisIcon className="btn" />
           </div>
-        )}
-        {/* Post comments */}
-        <p className="p-5 truncate"><span className="font-bold mr-2">{username}</span>{caption}</p>
-        {/* Post input box */}
-        {session && (
-          <form className="flex items-center p-4">
-            <FaceSmileIcon className="h-7"  />
-            <input className="border-none flex-1" type="text" placeholder="Enter your comment..." />
-            <button className="text-blue-400 font-bold">Post</button>
-          </form>
-        )}
+          <BookmarkIcon className="btn" />
+        </div>
+      )}
+      {/* Post comments */}
+      <p className="p-5 truncate">
+        <span className="font-bold mr-2">{username}</span>
+        {caption}
+      </p>
+      {/* Post input box */}
+      {session && (
+        <form className="flex items-center p-4">
+          <FaceSmileIcon className="h-7" />
+          <input
+            value={comment}
+            onChange={(event) => setComment(event.target.value)}
+            className="border-none flex-1"
+            type="text"
+            placeholder="Enter your comment..."
+          />
+          <button
+            type="submit"
+            onClick={sendComment}
+            disabled={!comment.trim()}
+            className="text-blue-400 font-bold disabled:text-blue-200"
+          >
+            Post
+          </button>
+        </form>
+      )}
     </div>
-  )
+  );
 }
